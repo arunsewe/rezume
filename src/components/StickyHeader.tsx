07c0faf0a8a299e891b5d769b3ddc40ea52bc223
Smurfz <charltonk.personal@gmail.com>
2024-10-29 17:21:44 +0800

    fix scroll viewport

--- src/components/StickyHeader.tsx
+++ src/components/StickyHeader.tsx
@@ -1,15 +1,43 @@
+"use client"
 import Link from "next/link";
 import React from "react";
 import { FaEnvelope, FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 
-const StickyHeader = () => {
+const StickyHeader: React.FC = () => {
+  // Function to handle smooth scroll to a specific section
+  const handleScroll = (id: string, offset = 0) => {
+    const section = document.getElementById(id);
+    if (section) {
+      const sectionPosition = section.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: sectionPosition - offset,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="sticky -top-2 pt-10 md:pt-24 bg-background p-6 z-10">
       <div className="flex flex-col md:flex-row gap-10 justify-between md:items-center">
         <div className="flex gap-10 items-center">
-          <a className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full" href="#about">About</a>
-          <a className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full" href="#experience">Experience</a>
-          <a className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full" href="#work">Work</a>
+          <button
+            className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full"
+            onClick={() => handleScroll("about", 200)}
+          >
+            About
+          </button>
+          <button
+            className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full"
+            onClick={() => handleScroll("experience", 200)}
+          >
+            Experience
+          </button>
+          <button
+            className="px-5 py-1 duration-200 bg-neutral-900 hover:bg-neutral-800 rounded-full"
+            onClick={() => handleScroll("work", 200)}
+          >
+            Work
+          </button>
         </div>
         <div className="flex gap-3 items-center">
           <Link href="https://linkedin.com/" target="_blank">
@@ -27,7 +55,7 @@ const StickyHeader = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StickyHeader
+export default StickyHeader;
