ea0bb94196e7e3dcc4e7655223b9adea3463c42b
khalifaomeiza <khalifabdul96@gmail.com>
2025-09-26 18:18:29 +0100

    update metadata

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "365a3e4b4e029faf"; }
+/******/ 		__webpack_require__.h = function() { return "b2321ce516b06543"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
